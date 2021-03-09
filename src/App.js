fa9bd6640c3edcf655d2f170b66e7d28d73b0b16
nguyenamyyy <60049918+nguyenamyyy@users.noreply.github.com>
2021-03-09 12:08:13 -0500

    finalized posts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Posts from './components/Posts.js'
 import { useState, useLayoutEffect } from 'react'
 import Header from './components/Header'
@@ -10,6 +9,7 @@ import ScrollToTopBtn from "./components/ScrollTop";
 import Scroll from './components/Scroll.js'
 
 function App() {
+  // hardcoded posts for front-end design purposes
   const [posts, setPosts] = useState([
     {
       id: 1,
@@ -26,13 +26,6 @@ function App() {
     },
     {
       id: 2,
-      title: 'Life Goes On',
-      quote: 'If this can no longer resonate with me / If this can no longer make my heart flutter / Perhaps, this will be how I die once',
-      gif: 'https://media.tenor.com/images/63fadc6e01f477acb05d70722205c624/tenor.gif',
-      description: 'A synth-pop track with...'
-    },
-    {
-      id: 3,
       title: 'Dis-ease - BTS',
       quote: "24 hours, that's plenty of time / I could sleep all day but there's still no problem / I think I should work till my body breaks",
       gif: 'https://64.media.tumblr.com/632764c235a36ae194cb1deea1ec0dd8/a79f2a82cb4ef3ab-39/s400x600/b4ccc58c1fcc6ddcf10ba1024b0f31573b22c1e6.gifv',
@@ -43,21 +36,29 @@ function App() {
       the track focuses on this personal disease (a play on the actual disease that has given us so much free time), the bridge offers hope to listeners: 'No night lasts forever / I've become stronger'."
     },
     {
+      id: 3,
+      title: 'Lorem ipsum dolor',
+      quote: 'Vel pharetra vel turpis nunc eget lorem dolor sed.',
+      gif: 'https://cdn2.scratch.mit.edu/get_image/gallery/28498744_170x100.png',
+      description: 'Lacus vel facilisis volutpat est velit egestas dui id. Donec ac odio tempor orci dapibus ultrices in iaculis. Sit amet nulla facilisi morbi tempus iaculis urna. Consectetur adipiscing elit pellentesque habitant morbi. Quis vel eros donec ac odio tempor. Massa tempor nec feugiat nisl pretium fusce. Turpis egestas maecenas pharetra convallis posuere morbi leo urna molestie. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Dictum fusce ut placerat orci nulla pellentesque. Blandit turpis cursus in hac habitasse platea dictumst quisque. Enim neque volutpat ac tincidunt vitae semper quis. Molestie at elementum eu facilisis sed odio morbi quis. Tincidunt id aliquet risus feugiat in ante metus dictum. Pharetra vel turpis nunc eget lorem dolor.'
+    },
+    {
       id: 4,
-      title: 'Good Days',
-      quote: 'If this can no longer resonate with me / If this can no longer make my heart flutter / Perhaps, this will be how I die once',
-      gif: 'https://media.tenor.com/images/63fadc6e01f477acb05d70722205c624/tenor.gif',
+      title: 'Lorem ipsum dolor',
+      quote: 'Vel pharetra vel turpis nunc eget lorem dolor sed.',
+      gif: 'https://cdn2.scratch.mit.edu/get_image/gallery/28498744_170x100.png',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl vel pretium lectus quam id leo. Id diam vel quam elementum. Diam donec adipiscing tristique risus nec feugiat. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Sem et tortor consequat id porta nibh venenatis. Auctor urna nunc id cursus metus aliquam eleifend. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Nunc id cursus metus aliquam eleifend. Bibendum est ultricies integer quis auctor elit sed. In hendrerit gravida rutrum quisque non tellus orci. Proin sed libero enim sed. Blandit aliquam etiam erat velit scelerisque in. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Adipiscing tristique risus nec feugiat in fermentum posuere urna. Pellentesque massa placerat duis ultricies lacus sed. Lobortis elementum nibh tellus molestie. Ullamcorper eget nulla facilisi etiam dignissim. Magna ac placerat vestibulum lectus mauris ultrices eros in cursus. Mi bibendum neque egestas congue quisque egestas.'
     },
     {
       id: 5,
-      title: 'Time Alone With You',
-      quote: 'If this can no longer resonate with me / If this can no longer make my heart flutter / Perhaps, this will be how I die once',
-      gif: 'https://media.tenor.com/images/63fadc6e01f477acb05d70722205c624/tenor.gif',
-      description: 'Grammy-nominated...'
+      title: 'Lorem ipsum dolor',
+      quote: 'Vel pharetra vel turpis nunc eget lorem dolor sed.',
+      gif: 'https://cdn2.scratch.mit.edu/get_image/gallery/28498744_170x100.png',
+      description: 'Lacus vel facilisis volutpat est velit egestas dui id. Donec ac odio tempor orci dapibus ultrices in iaculis. Sit amet nulla facilisi morbi tempus iaculis urna. Consectetur adipiscing elit pellentesque habitant morbi. Quis vel eros donec ac odio tempor. Massa tempor nec feugiat nisl pretium fusce. Turpis egestas maecenas pharetra convallis posuere morbi leo urna molestie. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Dictum fusce ut placerat orci nulla pellentesque. Blandit turpis cursus in hac habitasse platea dictumst quisque. Enim neque volutpat ac tincidunt vitae semper quis. Molestie at elementum eu facilisis sed odio morbi quis. Tincidunt id aliquet risus feugiat in ante metus dictum. Pharetra vel turpis nunc eget lorem dolor. '
     }
   ])
 
+  // selectedPost when button is clicked
   const [selectedPost, setSelectedPost] = useState({});
 
   return (
